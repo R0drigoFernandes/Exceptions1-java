b3692be85036197e56a62827261a41add85c1fe9
Controle de Gastos <rod.frnnds@gmail.com>
2025-05-18 23:45:09 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,10 +18,9 @@ public class Program{
         System.out.print("Check-out date (dd/MM/yyyy): ");
         Date checkOut = sdf.parse(sc.next());
         
-        if(!checkOut.after(checkIn)) {
-        	System.out.println("Error in reservation: Check-out date must be aftes check-in date!");
-        	
-        }else {
+        	if(!checkOut.after(checkIn)) {
+        		System.out.println("Error in reservation: Reservation dates for update must be future dates!");
+        	}else {
         	Reservation r1 = new Reservation(number, checkIn, checkOut);
         	System.out.println("Reservation: " + r1);
         	
@@ -32,20 +31,15 @@ public class Program{
             checkIn = sdf.parse(sc.next());
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
-            Date now = new Date();
             
-            if(checkIn.before(now) || checkOut.before(now)) {
-            	System.out.println("Error in reservation: Reservation dates for update must be future dates!");
-            	
-            }else if(!checkOut.after(checkIn)) {
-            	System.out.println("Error in reservation: Check-out date must be aftes check-in date!");
+            String error = r1.updateDates(checkIn, checkOut);
+            if(error != null) {
+            	System.out.println("Error in reservation: " + error);
             }else {
-            	r1.updateDates(checkIn, checkOut);
-            	System.out.println("Reservation: " + r1);
+            System.out.println("Reservation: " + r1);
             }
-        }
         
-        
+        	}
         
         
         
